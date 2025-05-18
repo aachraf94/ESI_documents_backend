4140ebe9a52f5fe297a7c5e6e7aa376e036be293
Abdelkebir Achraf <104693339+aachraf94@users.noreply.github.com>
2025-05-18 17:18:38 +0100

    fix: Update superuser creation logic to use email instead of username

--- build.sh
+++ build.sh
@@ -15,6 +15,6 @@ python manage.py migrate
 python manage.py shell << END
 from django.contrib.auth import get_user_model
 User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'adminpassword')
+if not User.objects.filter(email='admin@example.com').exists():
+    User.objects.create_superuser(email='admin@example.com', password='adminpassword')
 END
