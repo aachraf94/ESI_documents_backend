69fe6f61408dc1e7293710b741af18e103d035c9
Abdelkebir Achraf <104693339+aachraf94@users.noreply.github.com>
2025-05-18 17:22:49 +0100

    fix: Update superuser creation to include first and last name fields

--- build.sh
+++ build.sh
@@ -16,5 +16,5 @@ python manage.py shell << END
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(email='admin@example.com').exists():
-    User.objects.create_superuser(email='admin@example.com', password='adminpassword')
+    User.objects.create_superuser(email='admin@example.com', first_name='Admin', last_name='User', password='adminpassword')
 END
